6a3683b15bb48a382c037f69b836e7f3f330422f
ANIRUDDH-EKANSH-PANDEY <pandeyekansh1422@gmail.com>
2025-03-18 16:59:48 +0530

    Backend issue resolved

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -69,7 +69,7 @@ function Contact() {
 
     try {
       const response = await fetch(
-        "https://personal-portfolio-website-iquc.onrender.com/contact",
+        "https://personal-portfolio-website-iquc.onrender.com",
         {
           method: "POST",
           headers: {
